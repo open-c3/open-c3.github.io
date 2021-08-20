95ab2acf15f91c16460e0959f09acd6faa70f65f
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-08-20 18:58:30 +0800

    Automatic deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+rm -rf open-c3.github.io
+gitbook build
+git clone git@github.com:open-c3/open-c3.github.io.git
+
+rm -rf open-c3.github.io/*
+rsync -a _book/ open-c3.github.io/
+
+cd open-c3.github.io 
+git reset c40c88e77a8048a2efdae70dedcf6e302acdda26
+
+git add *
+git commit -m 'Automatic deployment'
+git push -f
+
+rm -rf ../open-c3.github.io
