57033ff9869097f2a16a031348c51942704c5746
lijinfeng2011 <lijinfeng2011@gmail.com>
2024-09-26 11:49:14 +0800

    Automatic deployment

---
+++ _code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cp _SUMMARY.md SUMMARY.md
+rm -rf open-c3-code_ open-c3-code
+git clone -b v2.6.0 git@github.com:open-c3/open-c3.git open-c3-code_
+find open-c3-code_ -type l -exec rm {} \;
+
+./c3mc-dtool-apidoc-make-sort > open-c3-code_/api.md
+./c3mc-dtool-c3mcdoc-make     > open-c3-code_/c3mcdoc.md
+./c3mc-dtool-todo-make        > open-c3-code_/c3todo.md
+
+find open-c3-code_  -name "*.md"|sed 's/[^\/]*.md$//'|sed 's/open-c3-code_/open-c3-code/'|xargs -i{} mkdir -p {}
+find open-c3-code_  -name "*.md" |xargs  -i echo {} {}|sed 's/ open-c3-code_/ open-c3-code/'|xargs -i{} bash -c  "cp {}"
+find open-c3-code   -name "*.md" |grep -v ISSUE_TEMPLATE |grep -v web-shell/private|grep -v open-c3-code/README.md |xargs  -i ./__code.sh {}|sort|awk '{print "    * [",$1,"](" ,$2,")"}' >> SUMMARY.md
+rm -rf open-c3-code_
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+rm -rf open-c3.github.io
+./build.sh
+
+#sed -i "s|../css/bootstrap.min.css|https://webapps1.chicago.gov/cdn/Bootstrap-3.3.7/bootstrap.min.css|g" `grep ../css/bootstrap.min.css -rl _book`
+#sed -i "s|../js/bootstrap.min.js|https://webapps1.chicago.gov/cdn/Bootstrap-3.3.7/bootstrap.min.js|g"    `grep ../js/bootstrap.min.js -rl _book`
+
+
+sed -i "s|../css/bootstrap.min.css|../css/bootstrap.min.css|g" `grep ../css/bootstrap.min.css -rl _book`
+sed -i "s|../js/bootstrap.min.js|../js/bootstrap.min.js|g"     `grep ../js/bootstrap.min.js -rl _book`
+
+git clone git@github.com:open-c3/open-c3.github.io.git
+
+rm -rf open-c3.github.io/*
+rsync -a _book/ open-c3.github.io/
+
+cd open-c3.github.io 
+git reset c40c88e77a8048a2efdae70dedcf6e302acdda26
+
+git add *
+git commit -m 'Automatic deployment'
+git push -f
+
+rm -rf ../open-c3.github.io
